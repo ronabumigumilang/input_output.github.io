aac018216de6c162656f555a74164e347e9de36f
ronabumigumilang <ronabumig@gmail.com>
2023-04-08 16:07:02 +0700

    project pertama input dan output sederhana(penambahan tombol reset)

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const output = document.querySelector('#output')
 const nama = document.querySelector('#nama')
 const umur = document.querySelector('#umur')
+let resetAll = document.querySelector('#resetAllInput')
+let inputs = document.querySelectorAll('input')
 function tampil() {
     // alert('tombol berfungsi!')
     // output.innerHTML = nama.value
     output.innerHTML = `Halo nama saya ${nama.value} dan umur saya ${umur.value}`
 }
+
+function reset() {
+    output.innerHTML = '';
+}
+
+resetAll.addEventListener('click', () => {
+    // inputs.forEach((input) => input.value = '')
+    inputs.forEach(function(input) {
+        input.value = ''
+    })
+})
